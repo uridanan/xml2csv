876bdcddb307fbfd544eb5c1e3fb5b566933a372
uridanan <uridanan@gmail.com>
2018-05-21 23:38:50 +0300

    Final
    Added documentation
    Cleanup unnecessary files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# xml2csv
+# Feel free to use at your convenience
+# This project was created to convert dita files into csv for database upload
+# The assumption is that we are interestd in elements with inner text only and that the entries should include tag, text and attributes
+# xml2csv is a rough draft in procedural programming
+# xmlconverter is a more object oriented approach, more suitable for re-use in other projects
+# Both will scan a folder for all dita or xml file and convert them into csv files
+# Enjoy
--- xmlconverter.py
+++ xmlconverter.py
@@ -4,13 +4,14 @@ from os import walk
 
 
 #-----------------------------------------------------------------------------------------------------------------------
+# Helper class to retrieve files in a specific folder
 class folder(object):
     path = ""
 
     def __init__(self, p):
         self.path = p
 
-
+    # Get all file names in a folder
     def getFileNames(self):
         f = []
         for (dirpath, dirnames, filenames) in walk(self.path):
@@ -19,6 +20,7 @@ class folder(object):
         return f
 
 
+    # Get all file names that match any the of the file extensions from a given set
     def getFilesByExtension(self, extensions):
         flist = []
         files = self.getFileNames()
@@ -28,17 +30,18 @@ class folder(object):
                     flist.append(f)
         return flist
 
-
+    # Get all xml files in the folder
     def getXMLFiles(self):
         return self.getFilesByExtension(["xml"])
 
-
+    # Get all xml dita in the folder
     def getDITAFiles(self):
         return self.getFilesByExtension(["dita"])
 #-----------------------------------------------------------------------------------------------------------------------
 
 
 #-----------------------------------------------------------------------------------------------------------------------
+# Helper class for string manipulations
 class myString(object):
     s = ""
 
@@ -48,12 +51,13 @@ class myString(object):
     def get(self):
         return self.s
 
-
+    # Remove all \n characters to flatten the string
     def stripNewLine(self):
         if (self.s == None):
             return ""
         return self.s.replace("\n", "")
 
+    # Returns true if the string is empty
     def isEmpty(self):
         if (self.s == None):
             return True
@@ -63,6 +67,7 @@ class myString(object):
 
 
 #-----------------------------------------------------------------------------------------------------------------------
+# Helper class for xml element manipulation
 class myElement(object):
     e= None
 
@@ -72,36 +77,36 @@ class myElement(object):
     def get(self):
         return self.e
 
-
-    # -------------------- end --------------------
-
+    # Returns true if the element has direct inner text (not including its sub-elements)
     def hasText(self):
         if(myString(self.e.text).isEmpty()):
             return False
         return True
 
+    # Returns all of the elements inner text
     def getInnerText(self):
         # t = ET.tostring(e, "us-ascii", "text").decode()
         return "".join(self.e.itertext())
 
+    # Returns a flat version of the inner text (after removing \n (new line) characters)
     def getCleanText(self):
         t = ""
         if(self.hasText()):
             t = self.getInnerText()
         return myString(t).stripNewLine()
 
-
+    # Format an attribute of the element as key=value
     def getKeyValuePair(self,a):
         return "=".join([a,self.e.attrib[a]])
 
-
+    # Return the element attributes as a string made of key=value pairs
     def getAttributes(self):
         attributes = []
         for a in self.e.attrib:
             attributes.append(self.getKeyValuePair(a))
         return ":".join(attributes)
 
-
+    # Returns the element tag, text and attributes as a comma separated string
     def getEntry(self):
         tag = myString(self.e.tag).stripNewLine()
         text = self.getCleanText()
@@ -113,6 +118,8 @@ class myElement(object):
 
 
 #-----------------------------------------------------------------------------------------------------------------------
+# Helper class for file manipulation
+# Loads xml files as input and exports to csv where each row is <tag>,<inner text>,<attributes>
 class xmlFile(object):
     inputFile = ""
     output = None
@@ -123,7 +130,7 @@ class xmlFile(object):
         self.inputFile = filename
         self.load()
 
-
+    # Load xml file using xml element tree
     def load(self):
         self.root = self.loadXML(self.inputFile)
 
@@ -132,31 +139,34 @@ class xmlFile(object):
     def loadXML(filename):
         return ET.parse(filename).getroot()
 
-
+    # Write a line to file and add carriage return
     @staticmethod
     def writeln(file, string):
         file.write(string)
         file.write('\n')
 
-
+    # Write headers to the csv file to describe the structure of the rows
     @staticmethod
     def getHeaders():
         return ",".join(["TAG", "TEXT", "ATTRIBUTES"])
 
 
+    # Print string to console, then to file (if available) with carriage return
     def printRow(self, row):
         print(row)
         if (self.output != None):
             self.writeln(self.output, row)
 
 
+    # Go over all sub elements of a given element
+    # Print elements that have inner text (directly, not in sub-elements)
     def iterateElements(self):
         for e in self.root.iter():
             me = myElement(e)
             if (me.hasText()):
                 self.printRow(me.getEntry())
 
-
+    # Export xml to csv file by print csv rows for each element that has inner text
     def toCSV(self):
         # Open output file
         outputFile = self.inputFile + ".csv"
@@ -185,21 +195,18 @@ class xmlFile(object):
 
 #-----------------------------------------------------------------------------------------------------------------------
 
-#Convert all xml / dita files in local folder
+#Convert all xml / dita files in a given folder
 def convertFolder(path):
-    files = folder(path).getDITAFiles()
+    #files = folder(path).getDITAFiles()
+    files = folder(path).getFilesByExtension([".dita",".xml"])
     print(files)
     for f in files:
         xmlFile(f).toCSV()
 
 
 def main():
+    # Convert all xml / dita files in local folder
     convertFolder(".")
 
 
 main()
-
-
-
-#TODO
-#4. documentation + README to share on github
